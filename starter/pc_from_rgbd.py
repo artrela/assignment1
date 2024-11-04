0c4e8ed8a7a79126898c51c9b9169d3b90aea23b
artrela <trelaalec@gmail.com>
2024-11-04 17:30:01 -0500

    Changes: ensure picture rightside up with a rotation

--- starter/pc_from_rgbd.py
+++ starter/pc_from_rgbd.py
@@ -6,6 +6,7 @@ import torch
 import tqdm
 from PIL import Image, ImageDraw
 import imageio
+from scipy.spatial.transform.rotation import Rotation
 
 def render_plant():
 
@@ -20,25 +21,27 @@ def render_plant():
         else:
             data[k] = v.to(device)
             
-    points1, rgb1 = unproject_depth_image(image=data['rgb1'], mask=data['mask1'], depth=data['depth1'], camera=data['cameras1'])
-    points2, rgb2 = unproject_depth_image(image=data['rgb2'], mask=data['mask2'], depth=data['depth2'], camera=data['cameras2'])
+    points1, rgb1 = unproject_depth_image(image=data['rgb1'], mask=data['mask1'], depth=data['depth1'].cpu(), camera=data['cameras1'])
+    points2, rgb2 = unproject_depth_image(image=data['rgb2'], mask=data['mask2'], depth=data['depth2'].cpu(), camera=data['cameras2'])
 
-    union_pts =  torch.from_numpy(np.concatenate((points1, points2), axis=0)).to(device)
-    union_rgb =  torch.from_numpy(np.concatenate((rgb1, rgb2), axis=0)).to(device)
+    union_pts =  torch.from_numpy(np.concatenate((points1.cpu(), points2.cpu()), axis=0)).to(device)
+    union_rgb =  torch.from_numpy(np.concatenate((rgb1.cpu(), rgb2.cpu()), axis=0)).to(device)
     
-    skip = 2
+    skip = 1
     pc1 = pytorch3d.structures.Pointclouds(points=points1[::skip].unsqueeze(0), features=rgb1[::skip].unsqueeze(0))
     pc2 = pytorch3d.structures.Pointclouds(points=points2[::skip].unsqueeze(0), features=rgb2[::skip].unsqueeze(0))
     pcU = pytorch3d.structures.Pointclouds(points=union_pts[::skip].unsqueeze(0), features=union_rgb[::skip].unsqueeze(0))
 
     renderer = get_points_renderer(
-        image_size=256, background_color=(1, 1, 1),
+        image_size=512, background_color=(1, 1, 1),
     )   
 
     renders = []
-    for azim in tqdm.tqdm(range(-60, 65, 5)):
+    R_180 = Rotation.from_euler('z', 180, degrees=True).as_matrix()
+    for azim in tqdm.tqdm(range(-360, 365, 5)):
 
-        R, T = pytorch3d.renderer.cameras.look_at_view_transform(dist=6.0, azim=azim, degrees=True)
+        R, T = pytorch3d.renderer.cameras.look_at_view_transform(dist=6.0, elev=0, azim=azim, degrees=True)
+        R = R @ R_180
 
         cameras = pytorch3d.renderer.FoVPerspectiveCameras(fov=60, R=R, T=T, device=device)
         
